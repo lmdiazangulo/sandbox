867f537b4652bb8272be71f843402893d3a81c08
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2023-05-25 12:39:28 +0200

    Testing cmake packaging

--- cmake_package_example/cmake_packaging/libexample_installation.cmake
+++ cmake_package_example/cmake_packaging/libexample_installation.cmake
@@ -7,10 +7,9 @@ install(
 )
 
 # target_include_directories(libexample PUBLIC ${CMAKE_INSTALL_PREFIX})
-
 include(CMakePackageConfigHelpers)
 write_basic_package_version_file(
-  "${CMAKE_CURRENT_BINARY_DIR}/libexample/libexampleConfigVersion.cmake"
+  "${CMAKE_CURRENT_BINARY_DIR}/libexampleConfigVersion.cmake"
   VERSION       ${libexample_VERSION}
   COMPATIBILITY AnyNewerVersion
 )
@@ -18,35 +17,36 @@ write_basic_package_version_file(
 # Export the targets (change namespace appropriately):
 export(
   EXPORT    libexampleTargets
-  FILE      "${CMAKE_CURRENT_BINARY_DIR}/libexample/libexampleTargets.cmake"
+  FILE      "${CMAKE_CURRENT_BINARY_DIR}/libexampleTargets.cmake"
 )
 
 # Copy the FooConfig.cmake to the build/Foo directory:
 configure_file(
-  libexampleConfig.cmake
-  "${CMAKE_CURRENT_BINARY_DIR}/libexample/libexampleConfig.cmake"
+  cmake_packaging/libexampleConfig.cmake
+  "${CMAKE_CURRENT_BINARY_DIR}/libexampleConfig.cmake"
   COPYONLY
 )
 
 # This is relative to CMAKE_INSTALL_PREFIX:
 # Change the location appropriately:
-set(ConfigPackageLocation lib/cmake/libexample)
+set(ConfigPackageLocation "libexample/")
+
 install(
   EXPORT      libexampleTargets
   FILE        libexampleTargets.cmake
-  DESTINATION ${ConfigPackageLocation}
+  DESTINATION ${ConfigPackageLocation}/lib/
 )
 
 # This also installs relative to CMAKE_INSTALL_PREFIX:
 install(
-  FILES       libexampleConfig.cmake
-              "${CMAKE_CURRENT_BINARY_DIR}/libexample/libexampleConfigVersion.cmake"
-  DESTINATION ${ConfigPackageLocation}
+  FILES       cmake_packaging/libexampleConfig.cmake
+              "${CMAKE_CURRENT_BINARY_DIR}/libexampleConfigVersion.cmake"
+  DESTINATION ${ConfigPackageLocation}/share/
 )
 
-# # This is for the source files, change location appropriately:
-# install(
-#   FILES ${libexample_SOURCE_DIR}
-#   DESTINATION ${CMAKE_INSTALL_PREFIX}/include}
-#   FILE_MATCHING_PATTERN "*.h"
-# )
+# This is for the source files, change location appropriately:
+install(
+  DIRECTORY "src/"
+  DESTINATION ${ConfigPackageLocation}/include/libexample/
+  FILES_MATCHING PATTERN "*.h"
+)
